7227c2bd17d9a3c579e27d411264215163eaabd2
debpriya3011 <debpriya3011@gmail.com>
2026-03-03 17:25:06 +0530

    fix: use tsc -b in all packages and enable declaration output

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -4,11 +4,5 @@ import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        {children}
-      </ThemeProvider>
-    </SessionProvider>
-  )
+  return <>{children}</>
 }
